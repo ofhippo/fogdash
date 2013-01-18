4c3ead46a26a3fc5d305a8a920e6eb09ad98e65b
Joel Birchler <joel@joelbirchler.com>
2013-01-17 16:44:37 -0800

    top stats update

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,19 @@
 (function() {
   
-  var socket = io.connect('http://localhost');
+  var statKeys = ['openCases', 'remaining', 'target']
+  
+  var socket = io.connect(location.protocol + '//' + location.hostname);
   socket.on('stats', function (data) {
     console.log(data);
+    
+    // TODO: iteration name
+    // TODO: case diagram
+    
+    if (data) {
+      _.each(statKeys, function(statKey) {
+        data[statKey] && $('#' + statKey).html(data[statKey]);
+      });
+    }
   });
   
 })();
